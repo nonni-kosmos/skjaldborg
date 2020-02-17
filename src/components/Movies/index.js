d50c4db6ff46b3c268a62eb0525955c1b59c9059
Nonni Linux <jglorange@gmail>
2020-02-17 12:47:21 +0000

    fix

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -3,10 +3,8 @@ import { Container } from "./Styled"
 
 // tech
 import React from "react"
-import useFirestoreCollection from "../../hooks/useFirestoreCollection"
 
 const Movies = () => {
-  //   const movies = useFirestoreCollection("movies")
   return <Container>Movies</Container>
 }
 
