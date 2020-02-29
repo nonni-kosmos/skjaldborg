e7adf50c3fd050431226edfb436162130c26c361
Nonni Linux <jglorange@gmail>
2020-02-29 15:52:16 +0000

    cms fix

--- src/components/TopImage/index.js
+++ src/components/TopImage/index.js
@@ -2,11 +2,7 @@ import React from "react"
 import { Container, Image } from "./styled"
 
 const Topimage = ({ image }) => {
-  return (
-    <Container>
-      <Image fluid={image.fluid}></Image>
-    </Container>
-  )
+  return <Container>{/* <Image fluid={image.fluid}></Image> */}</Container>
 }
 
 export default Topimage
