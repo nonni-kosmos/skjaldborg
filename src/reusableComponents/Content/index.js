a1605990ed4e688d0e674f605cdb480038ea27f6
Nonni Linux <jglorange@gmail>
2020-03-14 09:22:38 +0000

    translations

--- src/reusableComponents/Content/index.js
+++ src/reusableComponents/Content/index.js
@@ -1,8 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Container } from "./styled"
+import { useSelector } from "react-redux"
+
+const splitLang = html => {
+  return html.split("<p>-ENSKA-</p>")
+}
 
 const Content = ({ html }) => {
-  return <Container dangerouslySetInnerHTML={{ __html: html }}></Container>
+  const [lang, setLang] = useState(splitLang(html)[0])
+  const icelandic = useSelector(state => state.reducer.icelandic)
+
+  useEffect(() => {
+    let languages = splitLang(html)
+
+    if (icelandic) {
+      setLang(languages[0])
+    } else {
+      setLang(languages.length > 1 ? languages[1] : languages[0])
+    }
+  }, [icelandic, html])
+  return <Container dangerouslySetInnerHTML={{ __html: lang }}></Container>
 }
 
 export default Content
--- src/reusableComponents/Ernir/index.js
+++ src/reusableComponents/Ernir/index.js
@@ -4,11 +4,15 @@ import { graphql, StaticQuery } from "gatsby"
 import { Container, ImageContainer, Image } from "./styled"
 import PageTitle from "../PageTitle"
 import Content from "../Content"
+import { useSelector } from "react-redux"
 
 const Ernir = ({ data: { ernir } }) => {
+  const icelandic = useSelector(state => state.reducer.icelandic)
   return (
     <Container>
-      <PageTitle>Flugfélagið Ernir</PageTitle>
+      <PageTitle>
+        {icelandic ? ernir.frontmatter.title : ernir.frontmatter.title_en}
+      </PageTitle>
       <div className="content-wrap">
         <ImageContainer>
           <Image fluid={ernir.frontmatter.mynd.childImageSharp.fluid}></Image>
@@ -27,6 +31,7 @@ export default props => (
           html
           frontmatter {
             title
+            title_en
             mynd {
               childImageSharp {
                 fluid {
