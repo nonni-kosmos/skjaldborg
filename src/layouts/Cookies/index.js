381484558e0a2e43eec3dcd6286bd2e2df275a26
Nonni Linux <jglorange@gmail>
2020-04-03 13:51:31 +0000

    new lógós

---
+++ src/layouts/Cookies/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Popup, Text } from "./styled"
+
+const Cookies = () => {
+  return (
+    <Popup>
+      <Text></Text>
+    </Popup>
+  )
+}
+
+export default Cookies
---
+++ src/layouts/Cookies/styled.js
@@ -0,0 +1,4 @@
+import styled from "styled-components"
+
+export const Popup = styled.div``
+export const Text = styled.p``
