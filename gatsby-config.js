e1c2fb7595f8f74a437f094e41dd7cb24ecded0f
Nonni Linux <jglorange@gmail>
2020-04-03 17:48:01 +0000

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -167,5 +167,18 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-158316293-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   ],
 }
